f71de24134b62eeb39ec45f7dd0441d9d309d767
khalidchoukhmane <choukhmane.khalid@gmail.com>
2022-03-23 21:24:39 +0100

    full Material angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-material-ui';
+  displayedColumns: string[] = ['ProductName', 'Category', 'Date', 'Frashness', 'Price', 'Comment', 'Action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog, private api : ApiService) {}
+  
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+     width:'30%'
+    }).afterClosed().subscribe(val=>{
+      if(val == 'save'){
+        this.getAllProducts();
+      }
+    })
+  }
+
+  getAllProducts(){
+    this.api.getProduct()
+    .subscribe({
+      next:(res)=>{
+        this.dataSource= new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort
+      },
+      error:()=>{
+        alert("Error while fetching the Records!!");
+      }
+    })
+  }
+
+  editPriduct(row: any){
+    this.dialog.open(DialogComponent, {
+      width:'30%',
+      data:row
+    }).afterClosed().subscribe((val: any)=>{
+      if(val == 'update'){
+        this.getAllProducts();
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteProduct(id:number){
+    this.api.deleteProduct(id)
+    .subscribe({
+      next:(res)=>{
+        alert("Product deleted successfuly");
+        this.getAllProducts();
+      },
+      error:()=>{
+        alert("Error while deleting the product");
+      }
+    })
+  }
 }
